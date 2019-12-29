bebf3dafa74f94fafdfdab72b2d6db0c8abacf24
Katarine1 <kba.2879@gmail.com>
2019-12-28 21:29:22 -0300

    C# Bank Account App in Console

---
+++ App2_BankAccount/Model/Account.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App2_BankAccount.Model
+{
+    public class Account
+    {
+        private string Name { get; set; }
+        private string NumberAccout { get; set; }
+        private double Balance { get; set; }
+
+        public Account() { }
+
+        public Account(string name, string numberAccout, double balance)
+        {
+            Name = name;
+            NumberAccout = numberAccout;
+            Balance = balance;
+        }
+
+        public double deposit(double value)
+        {
+            return Balance += value;
+        }
+
+        public double withdraw(double value)
+        {
+            return Balance -= value + 0.2; //Plus 2% Rate
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + ", " + "Accout: " + NumberAccout + ", Balance : " + Balance + "\n";
+        }
+    }
+}
---
+++ App2_BankAccount/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using App2_BankAccount.Model;
+
+namespace App2_BankAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc = new Account();
+            Console.WriteLine();
+            Console.Write("\t\tQuantity : ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine("\t\tNumber " + (i + 1) + " :\n");
+                Console.Write("\t\tEnter your Name : ");
+                string name = Console.ReadLine();
+
+                Console.Write("\t\tEnter Account Number : ");
+                string numberAccount = Console.ReadLine();
+                                
+                Console.Write("\t\tDo you want to deposit (D) or withdraw (W)? Enter the letter : ");
+                char letter = char.Parse(Console.ReadLine());
+
+                if (letter == 'D' || letter == 'd')
+                {
+                    Console.Write("\n\t\tEnter the amount for deposit : ");
+                    double value = double.Parse(Console.ReadLine());
+                    double vl = acc.deposit(double.Parse(value.ToString("F")));
+                    acc = new Account(name, numberAccount, vl);
+                    Console.WriteLine("\n\t\t" + acc);
+                }
+                else if (letter == 'W' || letter == 'w')
+                {
+                    Console.Write("\n\t\tEnter the amount for withdrawal : ");
+                    double value = double.Parse(Console.ReadLine());
+                    double vl = acc.withdraw(double.Parse(value.ToString("F")));
+                    acc = new Account(name, numberAccount, vl);
+                    Console.WriteLine("\n\t\t" + acc);
+                }
+                else
+                {
+                    Console.WriteLine("\t\tError typing letter");
+                }
+            }
+        }
+    }
+}
